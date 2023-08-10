3b366ba46e29885e11702071d68d03daf5cd8986
Nandan Bhowmick <nandanbhow123@gmail.com>
2023-08-10 13:55:56 +0600

    Update NFA.cpp

--- NFA.cpp
+++ NFA.cpp
@@ -5,48 +5,72 @@ using namespace std;
 char transition[100][100][100];
 int numOfStates,numOfSymbols;
 
-void nfa(char startState,char finalState,string inputString,int curr,char *symbols,char *states){
-    if(startState=='X'){
+void nfa(char startState,char finalState,string inputString,int curr,char *symbols,char *states,string path)
+{
+    if(inputString[curr]!=NULL && startState=='X'){
+        cout<<"Rejected ";
+        cout<<path<<endl;
         return;
     }
-    int i,j,k,x;
-    for(i=curr;inputString[i];i++){
-        for(j=0;j<numOfStates;j++){
+    else
+    {
+        path+=startState;
+        if(inputString[curr]==NULL && startState!='C')
+        {
+            cout<<"Rejected ";
+            cout<<path<<endl;
+            return;
+        }
+        if(inputString[curr]==NULL && startState==finalState)
+        {
+            cout<<"Accepted ";
+            cout<<path<<endl;
+            return;
+        }
+        int i,j,k,x;
+        for(j=0; j<numOfStates; j++)
+        {
             if(startState==states[j])break;
         }
-        for(k=0;k<numOfSymbols;k++){
-            if(inputString[i]==symbols[k])break;
+        for(k=0; k<numOfSymbols; k++)
+        {
+            if(inputString[curr]==symbols[k])break;
         }
-        for(x=0;transition[j][k][x];x++){
-            nfa(states[j],finalState,inputString,i+1,symbols,states);
+        for(x=0; transition[j][k][x]; x++)
+        {
+            nfa(transition[j][k][x],finalState,inputString,curr+1,symbols,states,path);
         }
-
     }
 
 }
 
 int main()
 {
+    freopen("nfa.txt","r",stdin);
     int numOfTransitions,i,j,k;
     char startState,finalState;
     string inputString;
     cin>>numOfStates>>numOfSymbols;
 
     char states[numOfStates],symbols[numOfSymbols];
-    for(i=0;i<numOfStates;i++)cin>>states[i];
-    for(i=0;i<numOfSymbols;i++)cin>>symbols[i];
+    for(i=0; i<numOfStates; i++)cin>>states[i];
+    for(i=0; i<numOfSymbols; i++)cin>>symbols[i];
 
-    for(i=0;i<numOfStates;i++){
-        for(j=0;j<numOfSymbols;j++){
+    for(i=0; i<numOfStates; i++)
+    {
+        for(j=0; j<numOfSymbols; j++)
+        {
             cin>>numOfTransitions;
-            for(k=0;k<numOfTransitions;k++){
+            for(k=0; k<numOfTransitions; k++)
+            {
                 cin>>transition[i][j][k];
             }
-            transition[i][j][k]='X';
+            transition[i][j][k]=NULL;
         }
     }
     cin>>startState>>finalState;
     cin>>inputString;
+    nfa(startState,finalState,inputString,0,symbols,states,"");
 
 
     return 0;
